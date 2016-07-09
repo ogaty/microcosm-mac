284a7da0d9167d366eef1b6a4305a75d1c6b8510
ogaty <ogaty@admin>
2016-07-09 14:33:52 +0900

    initial

---
+++ microcosm-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  microcosm-mac
+//
+//  Created by Yuji Ogata on 2016/07/09.
+//  Copyright © 2016年 Yuji Ogata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ microcosm-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  microcosm-mac
+//
+//  Created by Yuji Ogata on 2016/07/09.
+//  Copyright © 2016年 Yuji Ogata. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
