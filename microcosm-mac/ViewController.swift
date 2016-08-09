b96d4eea03c7c9ffe8a67e8686507cd7f4350613
ogaty <ogaty@admin>
2016-08-10 00:40:57 +0900

    userData

--- microcosm-mac/ViewController.swift
+++ microcosm-mac/ViewController.swift
@@ -10,10 +10,21 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var userNameLbl: NSTextField!
+    @IBOutlet weak var userBirthStrLbl: NSTextField!
+    @IBOutlet weak var userBirthPlaceLbl: NSTextField!
+    @IBOutlet weak var userLatLbl: NSTextField!
+    @IBOutlet weak var userLngLbl: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        userNameLbl.stringValue = "現在時刻"
+        userBirthStrLbl.stringValue = "2016/8/9 12:00:00"
+        userBirthPlaceLbl.stringValue = "神奈川県川崎市高津区"
+        userLatLbl.stringValue = "38.0"
+        userLngLbl.stringValue = "139.0"
+
     }
 
     override var representedObject: AnyObject? {
