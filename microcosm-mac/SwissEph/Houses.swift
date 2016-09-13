02cb5afb76827ac8c9f7cfc868c9d73e969482bb
ogaty <ogaty@admin>
2016-09-13 23:42:52 +0900

    swe_houses

---
+++ microcosm-mac/SwissEph/Houses.swift
@@ -0,0 +1,21 @@
+//
+//  Houses.swift
+//  microcosm-mac
+//
+//  Created by Yuji Ogata on 2016/09/13.
+//  Copyright © 2016年 Yuji Ogata. All rights reserved.
+//
+
+import Cocoa
+
+class Houses: NSObject {
+    var cusp: [Double] = [0, 0, 0, 0]
+    var sundec: Double = 0
+    var ac: Double = 0
+    var mc: Double = 0
+    var vertex: Double = 0
+    var equasc: Double = 0
+    var coasc1: Double = 0
+    var coasc2: Double = 0
+    var polasc: Double = 0
+}
--- microcosm-mac/SwissEph/SwissHLib.swift
+++ microcosm-mac/SwissEph/SwissHLib.swift
@@ -17,4 +17,33 @@ class SwissHLib: NSObject {
         return x*x+y*y+z*z
     }
     
+    /* Reduce x modulo 360 degrees
+     */
+    func swe_degnorm(x: Double) -> Double
+    {
+        var y: Double = fmod(x, 360.0);
+        if (fabs(y) < 1e-13) {
+            y = 0;   /* Alois fix 11-dec-1999 */
+        }
+        if( y < 0.0 ) {
+            y += 360.0;
+        }
+        return y;
+    }
+    
+    /* Reduce x modulo TWOPI degrees
+     */
+    func swe_radnorm(x: Double) -> Double
+    {
+        var y: Double = fmod(x, TWOPI);
+        if (fabs(y) < 1e-13) {
+            y = 0;   /* Alois fix 11-dec-1999 */
+        }
+        if( y < 0.0 ) {
+            y += TWOPI;
+        } 
+        return y;
+    }
+
+    
 }
